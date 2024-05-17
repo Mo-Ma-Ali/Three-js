99ba747a28d9a88403bae9f00534b98ba1b161b1
ali <ali@klay.com>
2024-05-17 19:59:38 +0300

    start

---
+++ src/js/boat.js
@@ -0,0 +1,35 @@
+import vector from "./vector";
+import Const from "./const";
+class Entity{
+
+}
+class boat extends Entity{
+    constructor(
+        //  عوامل مؤثره
+
+    ){
+        //قيم ابتدائيه منستعملها بالفانكشن
+        // اسناد العوامل المؤثره لي فوق هون ولح نستعملها تحت بالقوانين عن طريق this
+        this.begin=vector.create(0,0);
+        this.g=0;
+    }
+    resetBoat()
+    {
+       // اعاده القارب لمكانه الابتدائي
+       this.begin=vector.create(0,0);
+    }
+   // حساب الجاذبيه 
+   calculate()
+   {
+    this.g=vector.create(0,
+        // الجاذبيه المحور y 
+        // القانون هون
+    );
+   }
+    
+
+   
+}
+class Water extends Entity{}
+
+class Waves extends Entity{}
